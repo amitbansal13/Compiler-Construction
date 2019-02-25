ab289e98413cf88a8894e97f0052d54d6c164c25
vedantpatwary <vedu.patwary@gmail.com>
2019-02-26 00:24:08 +0530

    Abhimanyu check

--- lexer.c
+++ lexer.c
@@ -1,21 +1,26 @@
 #include "HashTable.h"
 
 int line_no =0;
+int state = 0;
 
 char *nextToken(char *buf,int *index,int end){
-	int state = 0;
 	int i = *index;
 	switch(state){
 		case 0:
 			switch(buf[i]){
 				case '%' : 
+						state = 1;
 						i++;
 						while(i<end && buf[i]!='\n')
 							i++;
-						i++;
-						line_no++;
-						*index=i;
-						return "TK_COMMENT";
+						if(i==end);//need send some signal to fetch next buffer,will figure out later
+						else{
+							i++;
+							line_no++;
+							*index=i;
+							state = 2;
+							return "TK_COMMENT";
+						}
 				default:;
 			}
 		default:;

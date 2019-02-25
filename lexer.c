5328b8c6cf3ec82f9947eb772c3ae1c28a279f62
vedantpatwary <vedu.patwary@gmail.com>
2019-02-26 01:46:55 +0530

    Updated lexer.c

--- lexer.c
+++ lexer.c
@@ -1,59 +1,96 @@
 #include "HashTable.h"
+#include "library.h"
 
-int line_no =0;
+int lineNo =1;
 int state = 0;
 
-char *nextToken(char *buf,int *index,int end){
-
+TokenInfo nextToken(char *buf,int *index,int end){
+	TokenInfo token = (TokenInfo)malloc(sizeof(struct tokenInfo));
 	int i = *index;
 	while(1){
-	switch(state){
-		case 0:
-			switch(buf[i]){
-				case '%' : 
-					state = 1;
-					i++;
-					break;
-				case '0' ... '9':
-					state=7;
-					i++;
+		switch(state){
+			case 0:
+				switch(buf[i]){
+					case '%' : 						//move to state 1
+						state = 1;
+						i++;
+						break;
+					case '0' ... '9':				// move to state 7
+						i++;
+						state=7;
+						break;
+					
+					default:;							//need to update
+				}
+			case 1:
+				switch(buf[i]){
+					case '\n': 							//create_token and move to state 2
+						token->lineNo = lineNo;
+						strncpy(token->lexeme,buf,i-*index+1);
+						strcpy(token->Token,"TK_COMMENT"); 
+						i++;
+						lineNo++;
+						*index=i;
+						state = 2;
+						break;
+	
+					default:
+						while(i<end && buf[i]!='\n')	//stay in state 1 while iterating the inputs
+							i++;
+						break;
+				}
+			case 2:
+					return token;								//return token TK_COMMENT
+
+			case 7:												//switch to state 8 or 9
 					while(i<end && buf[i]<='9' && buf[i]>='0')
 						i++;
 					switch(buf[i]){
-						case '.':	state = 9;
+						case '.':	i++;state = 9;
 							break;
-						default:
+						default:	i++;state = 8;
+									
 					}
 					break;	
-				
-				default:;
-			}
-		case 1:
-			switch(buf[i]){
-
-				case '\n': 
-					i++;
-					line_no++;
-					*index=i;
-					state = 2;
-						break;
-
-				default:
-					while(i<end && buf[i]!='\n')
+			case 8:											//return token TK_NUM and retract
+					token->lineNo = lineNo;
+					strncpy(token->lexeme,buf,i-*index+1);
+					strcpy(token->Token,"TK_NUM"); 
+					i--;*index=i;
+					return token;	
+			case 9:
+				switch(buf[i]){
+					case '0' ... '9':
 						i++;
-		case 2:
-				return "TK_COMMENT";
-				break;
-				
-		default:;
-	}
+						state = 10;
+						break;
+					default:		;						//error it is
+				}
+			case 10:
+				switch(buf[i]){
+					case '0' ... '9':						//move to state 11
+						i++;
+						state = 11;
+						break;
+					default:	;							//error it is
+				}
+
+			case 11:											//return token TK_RNUM 
+					token->lineNo = lineNo;
+					strncpy(token->lexeme,buf,i-*index+1);
+					strcpy(token->Token,"TK_RNUM"); 
+					i++;*index=i;
+					return token;
+					
+			default:;
+		}
 	}
 
 	return NULL;
 }
-			
+	/*		
 int main(void){//helper main
 	int index =0;
 	nextToken("012",&index,3);
 
-}
+}*/

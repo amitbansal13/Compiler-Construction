275b37c6f16d3fc223b119889deccb98481f84ee
vedantpatwary <vedu.patwary@gmail.com>
2019-04-09 22:24:26 +0530

    pull

--- ast.h
+++
@@ -1,6 +0,0 @@
-#include "parserDef.h"
-
-void createAST(TreeNode root,int flag);
-void ASThelper(TreeNode node);
-TreeNode deleteN(TreeNode head,int index);
-void setParentChild(TreeNode parent,TreeNode childList);
